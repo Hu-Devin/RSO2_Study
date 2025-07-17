acfaf21b9816f1c9a13a172003089a70e4fc78ee
Devin <haifeng_njau@yeah.net>
2025-07-17 13:45:07 +0800

    added the learning service

---
+++ build/learning_interface/learning_interface__py/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/devin/ros2_ws/RSO2_Study/build/learning_interface/learning_interface__py
+# Build directory: /home/devin/ros2_ws/RSO2_Study/build/learning_interface/learning_interface__py
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
